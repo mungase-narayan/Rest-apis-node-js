08c1c3c33785e379e1aac41d5b5482a2a108760d
mungase-narayan <narayanmungase03@gmail.com>
2024-08-18 18:10:02 +0530

    add .enc variables

--- config/index.js
+++ config/index.js
@@ -8,5 +8,9 @@ module.exports = {
     DB_URL,
     JWT_SECRET,
     REFRESH_SECRET,
-    APP_URL
+    APP_URL,
+    SMTP_EMAIL,
+    SMTP_PASSWORD,
+    SMTP_HOST,
+    SMTP_PORT,
 } = process.env;
